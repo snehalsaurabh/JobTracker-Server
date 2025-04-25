6fdd560c411858363b2304cf72bf34db9cddca8a
Snehal Saurabh <snehalsaurabh.code@gmail.com>
2025-04-25 18:06:21 +0530

    Cors change

--- src/server.js
+++ src/server.js
@@ -13,10 +13,7 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Middleware
-app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-    credentials: true
-  }));
+app.use(cors());
 app.use(express.json());
 
 // Routes
